e742ff642149784aef94ab106eaaea911199c8e0
MrDyslexia <mhhernand@gmail.com>
2026-04-12 19:10:47 -0400

    Remove rutlib version 1.0.5 from cache, including all associated files and documentation

--- Frontend/~/.bun/install/cache/rutlib@1.0.5@@@1/lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare function cleanRut(rut: string): string;
-declare function validateRut(rut: string): boolean;
-declare function getLastDigitOfRut(rutNumbers: number): string;
-declare function formatRut(rut: string, withDots?: boolean): string;
-declare function generateRut(length?: number, formated?: boolean): string;
-declare function compareRuts(rut1: string, rut2: string): boolean;
-export { cleanRut, validateRut, getLastDigitOfRut, formatRut, generateRut, compareRuts };
-//# sourceMappingURL=index.d.ts.map
